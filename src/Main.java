8472d32798a299764553c393c950709a7aa91e7d
NaytonWilkerson <naytonwilkerson78@gmail.com>
2017-08-11 21:01:52 -0300

    Versão inicial do projeto

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		sc.useLocale(Locale.ENGLISH);
+		Locale.setDefault(new Locale("en", "us"));
+
+		Integer entrada1, entrada2;
+
+		entrada1 = sc.nextInt();
+
+		entrada2 = sc.nextInt();
+
+		
+
+		for (int j = 0; j < entrada2; j += 3) {
+
+		
+			for (int i = 0; i < entrada1; i++) {
+				
+				
+				
+				
+
+			}
+
+		}
+
+		sc.close();
+	}
+
+}
